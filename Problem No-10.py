97dac80690adcf7f3695fa1984ff9528f275bd30
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2022-10-10 22:12:59 +0530

    Update Problem No-10.py

--- Problem No-10.py
+++ Problem No-10.py
@@ -1,4 +1,9 @@
-# Write a python script to print first 10 multiples of N.
-for var in range(1,11,1):
-    print((5*var),end=" ")
-print()
+# Write a python script to display all prime numbers within a range.range start = 15 and end = 45.
+for e in range(15,45,1):
+    for k in range(2,e,1):
+        if e%k==0:
+            break
+        else:
+            continue
+    else:
+        print(e,end=" ")
