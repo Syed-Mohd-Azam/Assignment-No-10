3f66a30a71a096a1ef4a1c9ec9f769f1e2e8bc88
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2022-10-10 22:12:20 +0530

    Update Problem No-9.py

--- Problem No-9.py
+++ Problem No-9.py
@@ -1,4 +1,3 @@
 # Write a python script to print cubes of first N natural numbers.
-for var in range(1,(int(input("Enter the value of n to print the cubes of first n natural numbers : "))+1),1):
-    print((var*var*var),end=" ")
-print()
+n=int(input("Enter the value of N to print cubes of first N natural numbers : "))
+print([e**3 for e in range(1,n+1,1)])
